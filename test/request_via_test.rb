f4fccbfce5d5c62380ac419b9ce80001cb34f574
Rodrigo Serradura <rodrigo@ysimplicity.com>
2017-10-30 15:36:34 -0200

    Create new tests to assert URL parse errors

--- test/request_via_test.rb
+++ test/request_via_test.rb
@@ -4,4 +4,22 @@ class RequestViaTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RequestVia::VERSION
   end
+
+  def test_url_parse_error
+    assert_raises URI::InvalidURIError do
+      RequestVia::Get.('ftp://example.com')
+    end
+
+    assert_raises URI::InvalidURIError do
+      RequestVia::Post.('://example.com')
+    end
+
+    assert_raises URI::InvalidURIError do
+      RequestVia::Put.('//example.com')
+    end
+
+    assert_raises URI::InvalidURIError do
+      RequestVia::Delete.('tcp://example.com')
+    end
+  end
 end
