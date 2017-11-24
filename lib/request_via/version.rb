87b583a5f485011c7f5dfe44f5f5e95b10151109
Rodrigo Serradura <rodrigo@ysimplicity.com>
2017-11-24 17:29:53 -0200

    Build gem version with an anonymous function (lambda)

--- lib/request_via/version.rb
+++ lib/request_via/version.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 module RequestVia
-  module SemanticVersion
-    def self.call(major, minor, patch)
-      [ major, minor, patch ]
-        .join('.')
-        .freeze
-    end
-  end
-
-  VERSION = SemanticVersion.(0, 5, 1)
+  VERSION = (-> (major, minor, patch) {
+    [ major, minor, patch ]
+      .join('.')
+      .freeze
+  }).(0, 5, 1)
 end
